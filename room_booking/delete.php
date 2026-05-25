b519841fa97575c052b73646607577e53229cabe
lyajdmdbd <ainulaleeya08@gmail.com>
2026-05-25 15:42:02 +0800

    security: sanitized  parameter to prevent header injection

--- room_booking/delete.php
+++ room_booking/delete.php
@@ -3,7 +3,7 @@ require_once 'config.php';
 if(isset($_GET['id'])) {
     $id = intval($_GET['id']);
     // Tangkap tarikh dari URL supaya boleh hantar balik nanti
-    $date = $_GET['date'] ?? ''; 
+    $date = preg_replace('/[^0-9\-]/', '', $_GET['date'] ?? '');
     
     $conn->query("DELETE FROM bookings WHERE id = $id");
     
